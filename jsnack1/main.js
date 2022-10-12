c52ab93b81ebdc725791db3bcbe211d63287c780
UniversoCaotico1997 <loria.roberto97@gmail.com>
2022-10-12 11:40:11 +0200

    jsnack1

---
+++ jsnack1/main.js
@@ -0,0 +1,17 @@
+// L’utente inserisce due numeri in successione, con due prompt.
+// Il software stampa il maggiore.
+
+// STRUMENTI 
+// prompt 
+// condizionale >
+// if
+
+// L’utente inserisce due numeri in successione, con due prompt.
+const primoNumero  = Number( prompt(`scrivi un numero`))
+const secondoNumero= Number( prompt (`scrivi il secondo numero `))
+
+
+
+ if (primoNumero > secondoNumero) {
+    
+ }
