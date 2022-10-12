12bb8a2574e438c9bcd7202d11ef29ecfbc391d5
UniversoCaotico1997 <loria.roberto97@gmail.com>
2022-10-12 12:43:40 +0200

    jsnack3

---
+++ jsnack3/main.js
@@ -0,0 +1,12 @@
+// Il programma stampa la somma di tutti i numeri inseriti
+
+//STRUMENTI
+
+//ciclo for
+//condizionale +
+
+// Il software deve chiedere per 10 volte all’utente di inserire un numero.
+ for (let i = 0; i <= 10; i++) {
+    console.log(i);;
+    
+ }
