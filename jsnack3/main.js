136c76d78595df4890734c0d0003682bcb54f1d1
UniversoCaotico1997 <loria.roberto97@gmail.com>
2022-10-13 12:09:24 +0200

    jsnack 3 while

--- jsnack3/main.js
+++ jsnack3/main.js
@@ -6,7 +6,34 @@
 //condizionale +
 
 // Il software deve chiedere per 10 volte all’utente di inserire un numero.
- for (let i = 0; i <= 10; i++) {
-    console.log(i);;
+//  for (let i = 0; i <= 10; i++) {
+//     console.log(i);;
     
- }
+//  }
+
+const numbers = [
+   `1`,
+   `2`,
+   `3`,
+   `4`,
+   `5`,
+   `6`,
+   `7`,
+   `8`,
+   `9`,
+   `10`,
+]
+console.log(numbers);
+
+let i = 10;
+
+while (i <= numbers) {
+   const number = numbers[i];
+   console.log(number);
+
+   const userNumber = Number(prompt(`scrivi un numero`));
+
+   number+=userNumber
+
+   i++
+}
