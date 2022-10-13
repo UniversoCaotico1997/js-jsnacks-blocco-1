47cc67efffd8a24b7aea0eb4a0d660ac042c4abd
UniversoCaotico1997 <loria.roberto97@gmail.com>
2022-10-13 12:41:21 +0200

    jsnack4 con for

--- jsnack3/main.js
+++ jsnack3/main.js
@@ -6,34 +6,25 @@
 //condizionale +
 
 // Il software deve chiedere per 10 volte all’utente di inserire un numero.
-//  for (let i = 0; i <= 10; i++) {
-//     console.log(i);;
-    
-//  }
 
-const numbers = [
-   `1`,
-   `2`,
-   `3`,
-   `4`,
-   `5`,
-   `6`,
-   `7`,
-   `8`,
-   `9`,
-   `10`,
-]
-console.log(numbers);
+// let sum = 0;
+// for (let index = 0; index < 10; index++) {
+//   const userNumber = Number(prompt('Type a number'))
+//   sum += userNumber
+// }
 
-let i = 10;
+// console.log(sum);
 
-while (i <= numbers) {
-   const number = numbers[i];
-   console.log(number);
 
+
+let sum = 0;
+let i;
+while (i < 10) {
+   
    const userNumber = Number(prompt(`scrivi un numero`));
 
    number+=userNumber
 
-   i++
+   i++;
 }
+console.log(i);
---
+++ jsnack4/main.js
@@ -0,0 +1,51 @@
+// In un array sono contenuti i nomi degli invitati alla festa del grande Gatsby, 
+//chiedi all’utente il suo nome 
+// e comunicagli se può partecipare o no alla festa.
+
+// Strumenti 
+
+
+// Array 
+// prompt
+// ciclo for + Array 
+
+// creiamo una list adi partecipanti attravero Array 
+
+const listName = [
+    `Carolina`,
+    `Pippo`,
+    `Pluto`,
+    `Paperino`,
+    `Minni`,
+    `Topolino`
+];
+
+// console.log(listName);
+
+const nomeDaVisionare = prompt(`Scrivi il tuo nome`);
+
+// console.log(nomeDelPartecipante);
+
+// creaimo un ciclo for 
+
+let isValid = false;
+
+// Attravesro Array + ciclo visioniamo se la mail è presente nella lista 
+
+for (let i = 0; i < listName.length; i++) { 
+    const partecipante = listName [i]; 
+    // console.log(partecipante);
+    
+    // verifichiamo se il nome corrisponde 
+    if (partecipante === nomeDaVisionare){ 
+        // nome valido
+        isValid = true;  
+    } 
+    
+} 
+
+if (isValid) {
+    console.log(`Puoi entrare`);
+}else{
+    console.log(`Non puoi entare`);
+}
